39781a27239f4930c53bd0ef79501e01d2937620
DEMUYSERE Victor <victor.demu@gmail.com>
2023-11-22 14:34:11 +0100

    remade badges

--- src/components/PlaceBadge.jsx
+++ src/components/PlaceBadge.jsx
@@ -1,6 +1,33 @@
+import { useState } from 'react'
+
 function PlaceBadge({place_name}){
+    const [status, set_status] = useState("off"); // "off", "on", "muffled", "distant"
+    const [muffle_amount, set_muffle_amount] = useState(0.5);
+    const [distance, set_distance] = useState(50);
+
+    function switch_status(clicked_status){
+        if(status==clicked_status) set_status("off");
+        else set_status(clicked_status);
+    }
+
     return (
-        <button className="btn btn-primary">{place_name}</button>
+        <div className="card">
+            <div className='card-body d-flex flex-row gap-2 align-items-center'>
+                <h5 className="card-title mb-0 text-capitalize">{place_name}</h5>
+                <button onClick={()=>{switch_status("on")}}
+                    className={'btn btn'+(status=='on'?'':'-outline')+'-danger btn-sm'}>
+                    <i className="fa-solid fa-volume-high"></i>
+                </button>
+                <button onClick={()=>{switch_status("muffled")}}
+                    className={'btn btn'+(status=='muffled'?'':'-outline')+'-success btn-sm'}>
+                    <i className="fa-solid fa-volume-low"></i>
+                </button>
+                <button onClick={()=>{switch_status("distant")}}
+                    className={'btn btn'+(status=='distant'?'':'-outline')+'-primary btn-sm'}>
+                    <i className="fa-solid fa-people-arrows"></i>
+                </button>
+            </div>
+        </div>
     )
 }
 
